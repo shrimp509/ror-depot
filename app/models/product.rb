00bd4b17f1d5de25039caabdb9107873257f1c7c
SamHo <sam.ho@kdanmobile.com>
2020-06-10 15:50:53 +0800

    Finish chap9: Add to Cart btn

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  # validate each fields
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -9,5 +13,13 @@ class Product < ApplicationRecord
 
   def self.latest
   	order(:updated_at).last
+  end
+
+  private
+  def ensure_not_reference_by_any_line_item
+  	unless line_items.empty?
+  		errors.add(:base, 'Line Items present')
+  		throw :abort
+  	end
   end
 end
