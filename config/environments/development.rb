76c9e1f08bb41f6b88c717129ceda32b4e8b60cf
SamHo <sam.ho@kdanmobile.com>
2020-06-11 16:05:45 +0800

    Finish chap13: mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Depot::Application.configure do
+  	config.action_mailer.delivery_method = :smtp
+  	config.action_mailer.smtp_settings = {
+  		address: "smtp.gmail.com",
+  		port: 587,
+  		domain: "domain.of.sender.net",
+  		authentication: "plain",
+  		user_name: "dave",
+  		password: "password",
+  		enable_starttls_auto: true
+  	}
+  end
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
