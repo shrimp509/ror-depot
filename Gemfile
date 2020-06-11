aa04f05e1e28be23048e83cc6074c38b33625dcf
SamHo <sam.ho@kdanmobile.com>
2020-06-11 10:05:28 +0800

    Finish chap11: cowork with js

--- Gemfile
+++ Gemfile
@@ -52,3 +52,11 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# lookup document
+gem 'pry'
+gem 'pry-rails'
+
+# jquery
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,8 +32,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
